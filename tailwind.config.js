b1626ada3a1f41af2b6e431d7bfd999a7ca338bf
jiathinhj <hoangvugiathinh@gmail.com>
2024-10-13 15:53:51 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    darkMode: 'class',
+    theme: {
+      fontFamily: {
+        display: ['Open Sans', 'sans-serif'],
+        body: ['Open Sans', 'sans-serif'],
+      },
+      extend: {
+        fontSize: {
+          14: '14px',
+        },
+        backgroundColor: {
+          'main-bg': '#FAFBFB',
+          'main-dark-bg': '#20232A',
+          'secondary-dark-bg': '#33373E',
+          'light-gray': '#F7F7F7',
+          'half-transparent': 'rgba(0, 0, 0, 0.5)',
+        },
+        borderWidth: {
+          1: '1px',
+        },
+        borderColor: {
+          color: 'rgba(0, 0, 0, 0.1)',
+        },
+        width: {
+          400: '400px',
+          760: '760px',
+          780: '780px',
+          800: '800px',
+          1000: '1000px',
+          1200: '1200px',
+          1400: '1400px',
+        },
+        height: {
+          80: '80px',
+        },
+        minHeight: {
+          590: '590px',
+        },
+        backgroundImage: {
+          'hero-pattern':
+            "url('https://img.freepik.com/premium-vector/user-interacts-with-colorful-online-shopping-landing-page-featuring-various-shopping-options-graphics-online-shopping-landing-page_538213-148389.jpg?w=1480')",
+        },
+      },
+    },
+    plugins: [],
+  };
